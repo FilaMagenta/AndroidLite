d6ce4bc2c29f0c3aa35a3147b56adfc8ae5b2009
Arnau Mora <arnyminer.z@gmail.com>
2023-03-12 15:45:51 +0100

    Added `OutlinedDropdownField`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/filmagentaproto/ui/screens/SettingsScreen.kt
+++ app/src/main/java/com/arnyminerz/filmagentaproto/ui/screens/SettingsScreen.kt
@@ -4,19 +4,12 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -26,6 +19,7 @@ import com.arnyminerz.filmagentaproto.BuildConfig
 import com.arnyminerz.filmagentaproto.Locator
 import com.arnyminerz.filmagentaproto.R
 import com.arnyminerz.filmagentaproto.service.LanguageChangeReceiver
+import com.arnyminerz.filmagentaproto.ui.components.OutlinedDropdownField
 import com.arnyminerz.filmagentaproto.utils.capitalized
 
 @Composable
@@ -37,48 +31,22 @@ fun SettingsScreen() {
                 .fillMaxWidth()
                 .weight(1f)
         ) {
-            var expanded by remember { mutableStateOf(false) }
             val languages by LanguageChangeReceiver.currentLanguage.observeAsState()
 
-            ExposedDropdownMenuBox(
-                expanded = expanded,
-                onExpandedChange = { expanded = !expanded }) {
-                OutlinedTextField(
-                    value = languages
-                        ?.firstOrNull()
-                        ?.let { it.getDisplayName(it).capitalized(it) }
-                        ?: stringResource(R.string.settings_language_default),
-                    onValueChange = {},
-                    readOnly = true,
-                    trailingIcon = {
-                        ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
-                    },
-                    label = { Text(stringResource(R.string.settings_language)) },
-                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 8.dp, vertical = 4.dp)
-                        .menuAnchor(),
+            OutlinedDropdownField(
+                value = languages
+                    ?.firstOrNull()
+                    ?.let { it.getDisplayName(it).capitalized(it) }
+                    ?: stringResource(R.string.settings_language_default),
+                options = Locator.Locales.toList(),
+                label = stringResource(R.string.settings_language),
+            ) { locale ->
+                LanguageChangeReceiver.currentLanguage.postValue(
+                    listOf(locale)
                 )
-                ExposedDropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false },
-                ) {
-                    Locator.Locales.forEach { locale ->
-                        DropdownMenuItem(
-                            text = { Text(locale.getDisplayName(locale).capitalized(locale)) },
-                            onClick = {
-                                LanguageChangeReceiver.currentLanguage.postValue(
-                                    listOf(locale)
-                                )
-                                AppCompatDelegate.setApplicationLocales(
-                                    LocaleListCompat.create(locale)
-                                )
-                                expanded = false
-                            },
-                        )
-                    }
-                }
+                AppCompatDelegate.setApplicationLocales(
+                    LocaleListCompat.create(locale)
+                )
             }
         }
         Text(
