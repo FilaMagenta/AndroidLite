254bbbc0dc254dc7b8c40dbe926433b7f51cbf09
Arnau Mora <arnyminer.z@gmail.com>
2023-03-12 13:38:06 +0100

    Removed annotation
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/filmagentaproto/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/arnyminerz/filmagentaproto/ui/screens/LoginScreen.kt
@@ -4,12 +4,10 @@ import android.util.Log
 import androidx.annotation.WorkerThread
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -106,12 +104,6 @@ fun LoginScreen(
         ) {
             Text(stringResource(R.string.login_action))
         }
-        Text(
-            text = stringResource(R.string.login_commerce),
-            style = MaterialTheme.typography.labelLarge,
-            modifier = Modifier.fillMaxWidth().padding(horizontal = 8.dp),
-            color = MaterialTheme.colorScheme.onBackground.copy(alpha = .5f),
-        )
     }
 }
 
