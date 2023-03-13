4d12b37e902d3080e97e9fa26e16b83266673935
Arnau Mora <arnyminer.z@gmail.com>
2023-03-13 12:34:16 +0100

    Changed options chooser
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/filmagentaproto/ui/dialogs/EventBottomSheet.kt
+++ app/src/main/java/com/arnyminerz/filmagentaproto/ui/dialogs/EventBottomSheet.kt
@@ -1,13 +1,15 @@
 package com.arnyminerz.filmagentaproto.ui.dialogs
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
@@ -17,15 +19,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.arnyminerz.filmagentaproto.R
 import com.arnyminerz.filmagentaproto.database.data.woo.Event
-import com.arnyminerz.filmagentaproto.ui.components.OutlinedDropdownField
 
 @Composable
 @ExperimentalMaterial3Api
@@ -54,31 +54,65 @@ fun EventBottomSheet(
             ?.filter { it.visible && it.options.isNotEmpty() }
             ?.sortedBy { it.position }
             ?.forEach { attribute ->
-                var option by remember { mutableStateOf(attribute.options[0]) }
+                var selectedOption by remember { mutableStateOf(attribute.options[0]) }
 
-                OutlinedDropdownField(
-                    value = option,
-                    label = attribute.name,
-                    options = attribute.options,
-                ) { option = it }
+                Text(
+                    text = attribute.name,
+                    style = MaterialTheme.typography.labelLarge,
+                    modifier = Modifier.padding(horizontal = 8.dp),
+                )
+                OutlinedCard(
+                    Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 8.dp, vertical = 4.dp)
+                ) {
+                    for (option in attribute.options) {
+                        ListItem(
+                            headlineContent = { Text(option) },
+                            colors = ListItemDefaults.colors(
+                                containerColor = if (option == selectedOption)
+                                    MaterialTheme.colorScheme.surfaceVariant
+                                else
+                                    Color.Unspecified,
+                            ),
+                            modifier = Modifier
+                                .clickable {
+                                    selectedOption = option
+                                }
+                        )
+                    }
+                }
+
+                // FIXME: Add dropdown when fixed
+                // https://issuetracker.google.com/issues/272882500
+                // https://issuetracker.google.com/issues/271430136
+                // OutlinedDropdownField(
+                //     value = option,
+                //     label = attribute.name,
+                //     options = attribute.options,
+                // ) { option = it }
             }
 
-        var notes by remember { mutableStateOf("") }
+        // var notes by remember { mutableStateOf("") }
 
-        OutlinedTextField(
-            value = notes,
-            onValueChange = { notes = it },
-            label = { Text(stringResource(R.string.events_modal_notes)) },
-            keyboardOptions = KeyboardOptions(
-                capitalization = KeyboardCapitalization.Sentences,
-                keyboardType = KeyboardType.Text,
-                autoCorrect = true,
-            ),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = 8.dp)
-                .padding(bottom = 8.dp),
-        )
+        // FIXME: Change when fixed
+        // https://issuetracker.google.com/issues/268380384
+        // https://issuetracker.google.com/issues/272483584
+        // https://issuetracker.google.com/issues/261572786
+        // OutlinedTextField(
+        //     value = notes,
+        //     onValueChange = { notes = it },
+        //     label = { Text(stringResource(R.string.events_modal_notes)) },
+        //     keyboardOptions = KeyboardOptions(
+        //         capitalization = KeyboardCapitalization.Sentences,
+        //         keyboardType = KeyboardType.Text,
+        //         autoCorrect = true,
+        //     ),
+        //     modifier = Modifier
+        //         .fillMaxWidth()
+        //         .padding(horizontal = 8.dp)
+        //         .padding(bottom = 8.dp),
+        // )
 
         Button(
             onClick = onSubmit,
