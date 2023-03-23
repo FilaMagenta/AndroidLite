f738c5f7418833124e67cf93c5cadfa52533c78c
Arnau Mora <arnyminer.z@gmail.com>
2023-03-24 00:10:13 +0100

    QR code rounded corners
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/filmagentaproto/activity/EventActivity.kt
+++ app/src/main/java/com/arnyminerz/filmagentaproto/activity/EventActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.DeleteForever
@@ -43,6 +44,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
@@ -310,7 +312,6 @@ class EventActivity : AppCompatActivity() {
             )
 
             val qr = remember { order.getQRCode(customer) }
-            // TODO: Rounded corners
             Image(
                 bitmap = qr.asImageBitmap(),
                 contentDescription = "",
@@ -318,7 +319,8 @@ class EventActivity : AppCompatActivity() {
                     .fillMaxWidth()
                     .height(300.dp)
                     .padding(8.dp)
-                    .padding(bottom = 12.dp),
+                    .padding(bottom = 12.dp)
+                    .clip(RoundedCornerShape(12.dp)),
                 contentScale = ContentScale.Fit,
             )
         }
@@ -362,7 +364,7 @@ class EventActivity : AppCompatActivity() {
                 Log.d(TAG, "Searching for order...")
                 order = orders
                     .filter { it.customerId == customer.id }
-                    .firstOrNull { order -> order.items.find { it.productId == event.id } != null }
+                    .firstOrNull { item -> item.items.find { it.productId == event.id } != null }
                     .also {
                         if (it == null)
                             Log.e(TAG, "Could not find an order from event and customer!")
