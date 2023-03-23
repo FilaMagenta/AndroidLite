c26fb8ac4b74f0d5a421d7602727308562f5467d
Arnau Mora <arnyminer.z@gmail.com>
2023-03-23 20:48:11 +0100

    Added shortcut creation
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/filmagentaproto/App.kt
+++ app/src/main/java/com/arnyminerz/filmagentaproto/App.kt
@@ -3,27 +3,30 @@ package com.arnyminerz.filmagentaproto
 import android.accounts.Account
 import android.accounts.AccountManager
 import android.accounts.OnAccountsUpdateListener
-import android.annotation.SuppressLint
 import android.app.Application
-import android.app.PendingIntent
 import android.content.Intent
 import android.util.Log
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
+import androidx.core.content.pm.ShortcutManagerCompat.FLAG_MATCH_DYNAMIC
+import androidx.core.graphics.drawable.IconCompat
 import androidx.core.os.HandlerCompat
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
-import androidx.work.WorkInfo
+import com.arnyminerz.filmagentaproto.Shortcuts.ADMIN_PANEL_ID
+import com.arnyminerz.filmagentaproto.Shortcuts.ADMIN_PANEL_LABEL_RES
 import com.arnyminerz.filmagentaproto.account.Authenticator
+import com.arnyminerz.filmagentaproto.activity.AdminActivity
+import com.arnyminerz.filmagentaproto.database.data.woo.Customer
+import com.arnyminerz.filmagentaproto.database.data.woo.ROLE_ADMINISTRATOR
+import com.arnyminerz.filmagentaproto.database.local.AppDatabase
+import com.arnyminerz.filmagentaproto.database.local.WooCommerceDao
 import com.arnyminerz.filmagentaproto.storage.SELECTED_ACCOUNT
 import com.arnyminerz.filmagentaproto.storage.dataStore
-import com.arnyminerz.filmagentaproto.utils.PermissionsUtils.hasNotificationPermission
 import com.arnyminerz.filmagentaproto.utils.doAsync
 import io.sentry.Sentry
 import io.sentry.android.core.SentryAndroid
 import io.sentry.protocol.User
-import kotlin.random.Random
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.map
 
@@ -39,7 +42,14 @@ class App : Application(), OnAccountsUpdateListener, FlowCollector<Int> {
     val accounts: LiveData<List<Account>>
         get() = accountsLiveData
 
+    private val customerLiveData = MutableLiveData<Customer>()
+
+    val customer: LiveData<Customer>
+        get() = customerLiveData
+
     private var selectedAccountIndex = 0
+
+    private lateinit var wooCommerceDao: WooCommerceDao
 
     override fun onCreate() {
         super.onCreate()
@@ -51,6 +61,9 @@ class App : Application(), OnAccountsUpdateListener, FlowCollector<Int> {
 
         // Schedule the SyncWorker to run automatically
         SyncWorker.schedule(this)
+
+        // Instantiate the Dao
+        wooCommerceDao = AppDatabase.getInstance(this).wooCommerceDao()
 
         // Start observing the currently selected account index
         doAsync {
@@ -74,6 +87,7 @@ class App : Application(), OnAccountsUpdateListener, FlowCollector<Int> {
         selectedAccountIndex = value
 
         updateUserId()
+        updateAdminQuickAccessShortcut()
     }
 
     override fun onAccountsUpdated(accounts: Array<out Account>?) {
@@ -82,6 +96,7 @@ class App : Application(), OnAccountsUpdateListener, FlowCollector<Int> {
         accountsLiveData.postValue(filteredAccounts)
 
         updateUserId()
+        updateAdminQuickAccessShortcut()
     }
 
     /**
@@ -101,5 +116,52 @@ class App : Application(), OnAccountsUpdateListener, FlowCollector<Int> {
         Sentry.setUser(user)
 
         Log.i(TAG, "Updated user reference to: ${account.name} ($dni, $customerId)")
+    }
+
+    /**
+     * Adds or removes the admin panel shortcut according to the currently selected user.
+     */
+    private fun updateAdminQuickAccessShortcut() {
+        doAsync {
+            dataStore
+                .data
+                .map { preferences -> preferences[SELECTED_ACCOUNT] ?: 0 }
+                .map { index ->
+                    accounts.value?.get(index)?.let { account ->
+                        val customerId: Long = am.getUserData(account, "customer_id")
+                            ?.toLongOrNull() ?: return@let null
+                        wooCommerceDao.getAllCustomers().find { it.id == customerId }
+                    }
+                }
+                .collect { customer ->
+                    // Update the customer state
+                    customerLiveData.postValue(customer)
+
+                    // If no customer, or not an admin, remove
+                    if (customer == null || customer.role != ROLE_ADMINISTRATOR)
+                        return@collect ShortcutManagerCompat.removeDynamicShortcuts(
+                            this@App,
+                            listOf(ADMIN_PANEL_ID),
+                        )
+                    val shortcuts = ShortcutManagerCompat.getShortcuts(this@App, FLAG_MATCH_DYNAMIC)
+
+                    // Do not create multiple times
+                    if (shortcuts.find { it.id == ADMIN_PANEL_ID } != null)
+                        return@collect
+
+                    // Create the shortcut for the admin panel
+                    val shortcut = ShortcutInfoCompat.Builder(this@App, ADMIN_PANEL_ID)
+                        .setShortLabel(getString(ADMIN_PANEL_LABEL_RES))
+                        .setLongLabel(getString(ADMIN_PANEL_LABEL_RES))
+                        .setIcon(IconCompat.createWithResource(this@App, R.mipmap.ic_launcher))
+                        .setIntent(
+                            Intent(this@App, AdminActivity::class.java).apply {
+                                action = Intent.ACTION_VIEW
+                            }
+                        )
+                        .build()
+                    ShortcutManagerCompat.pushDynamicShortcut(this@App, shortcut)
+                }
+        }
     }
 }
