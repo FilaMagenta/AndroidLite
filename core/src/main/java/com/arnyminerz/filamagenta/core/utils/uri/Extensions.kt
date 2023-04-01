d5fcec5e6a3b098cf347c3c75d035d4d8de1d88d
Arnau Mora <arnyminer.z@gmail.com>
2023-04-01 17:59:44 +0200

    Fixed query parameters extraction
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- core/src/main/java/com/arnyminerz/filamagenta/core/utils/uri/Extensions.kt
+++ core/src/main/java/com/arnyminerz/filamagenta/core/utils/uri/Extensions.kt
@@ -2,4 +2,13 @@ package com.arnyminerz.filamagenta.core.utils.uri
 
 import java.net.URI
 
-fun URI.buildUpon(): UriBuilder = UriBuilder(scheme, host, rawPath.split('/').filter { it.isNotBlank() })
+fun URI.buildUpon(): UriBuilder =
+    UriBuilder(
+        scheme,
+        host,
+        rawPath.split('/').filter { it.isNotBlank() },
+        query.split('&')
+            .associate { pair ->
+                pair.split('=').let { it[0] to it.subList(1, it.size).joinToString("=") }
+            }
+    )
--- core/src/test/java/com/arnyminerz/filamagenta/core/TestUriBuilder.kt
+++ core/src/test/java/com/arnyminerz/filamagenta/core/TestUriBuilder.kt
@@ -11,23 +11,31 @@ class TestUriBuilder {
     fun test_builder() {
         val uriBuilder = UriBuilder(host = "example.com")
             .appendPath("test")
+            .appendQueryParameter("key", "value")
         assertEquals("https", uriBuilder.protocol)
         assertEquals("example.com", uriBuilder.host)
         assertEquals(listOf("test"), uriBuilder.path)
-        assertEquals(0, uriBuilder.query.size)
+        assertEquals(mapOf("key" to "value"), uriBuilder.query)
         val uri = uriBuilder.build()
-        assertEquals("https://example.com/test", uri.toString())
+        assertEquals("https://example.com/test?key=value", uri.toString())
     }
 
     @Test
     fun test_buildUpon() {
-        val srcUri = URI.create("https://example.com/test")
+        val srcUri = URI.create("https://example.com/test?key=value")
         val uriBuilder = srcUri.buildUpon()
         assertEquals("https", uriBuilder.protocol)
         assertEquals("example.com", uriBuilder.host)
         assertEquals(listOf("test"), uriBuilder.path)
-        assertEquals(0, uriBuilder.query.size)
+        assertEquals(mapOf("key" to "value"), uriBuilder.query)
         val uri = uriBuilder.build()
-        assertEquals("https://example.com/test", uri.toString())
+        assertEquals("https://example.com/test?key=value", uri.toString())
+    }
+
+    @Test
+    fun test_buildUpon_query() {
+        val srcUri = URI.create("https://example.com/test?key=value&another==complex=")
+        val uriBuilder = srcUri.buildUpon()
+        assertEquals(mapOf("key" to "value", "another" to "=complex="), uriBuilder.query)
     }
 }
