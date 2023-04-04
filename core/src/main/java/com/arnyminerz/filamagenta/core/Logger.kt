ac8ba6b5312df307b4956895d2f7945acdc61a3c
Arnau Mora <arnyminer.z@gmail.com>
2023-04-04 12:28:20 +0200

    Improved log messages
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ core/src/main/java/com/arnyminerz/filamagenta/core/Logger.kt
@@ -0,0 +1,30 @@
+package com.arnyminerz.filamagenta.core
+
+import com.arnyminerz.filamagenta.core.utils.ANSI_BLUE
+import com.arnyminerz.filamagenta.core.utils.ANSI_CYAN
+import com.arnyminerz.filamagenta.core.utils.ANSI_RED
+import com.arnyminerz.filamagenta.core.utils.ANSI_RESET
+import com.arnyminerz.filamagenta.core.utils.ANSI_YELLOW
+
+object Logger {
+
+    fun v(tag: String, message: String, vararg formatArgs: Any?) {
+        println(ANSI_BLUE + "$tag/V: " + ANSI_RESET + message.format(formatArgs))
+    }
+
+    fun d(tag: String, message: String, vararg formatArgs: Any?) {
+        println(ANSI_BLUE + "$tag/D: " + ANSI_RESET + message.format(formatArgs))
+    }
+
+    fun i(tag: String, message: String, vararg formatArgs: Any?) {
+        println(ANSI_CYAN + "$tag/I: " + ANSI_RESET + message.format(formatArgs))
+    }
+
+    fun w(tag: String, message: String, vararg formatArgs: Any?) {
+        println(ANSI_YELLOW + "$tag/W: " + ANSI_RESET + message.format(formatArgs))
+    }
+
+    fun e(tag: String, message: String, vararg formatArgs: Any?) {
+        System.err.println(ANSI_RED + "$tag/E: " + ANSI_RESET + message.format(formatArgs))
+    }
+}
