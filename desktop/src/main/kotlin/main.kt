adb70aa078e8520e196ba93723099620f603a89a
Arnau Mora <arnyminerz@proton.me>
2023-04-05 10:15:03 +0200

    Added Sentry
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- desktop/src/main/kotlin/main.kt
+++ desktop/src/main/kotlin/main.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.application
 import com.arnyminerz.filamagenta.core.Logger
+import com.arnyminerz.filamagenta.core.resources.LocalCredentialsProvider
 import com.arnyminerz.filamagenta.core.utils.doAsync
 import com.arnyminerz.filamagenta.desktop.localization.Translations
 import com.arnyminerz.filamagenta.desktop.storage.LocalPropertiesStorage
@@ -11,6 +12,7 @@ import com.arnyminerz.filamagenta.desktop.storage.Properties.LANGUAGE
 import com.arnyminerz.filamagenta.desktop.storage.Properties.USER_TOKEN
 import com.arnyminerz.filamagenta.desktop.ui.window.LoginWindow
 import com.arnyminerz.filamagenta.desktop.ui.window.MainWindow
+import io.sentry.Sentry
 import java.util.Locale
 
 private const val TAG = "Main"
@@ -21,6 +23,16 @@ fun main() {
     LocalPropertiesStorage[LANGUAGE]?.let {
         Translations.setFallback(it)
     } ?: Translations.setFallback(Locale.ENGLISH)
+
+    Logger.i(TAG, "Initializing Sentry...")
+    Sentry.init { options ->
+        options.dsn = LocalCredentialsProvider["SENTRY_DSN"]
+        // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+        // We recommend adjusting this value in production.
+        options.tracesSampleRate = 1.0
+        // When first trying Sentry it's good to see what the SDK is doing:
+        options.isDebug = true
+    }
 
     application(exitProcessOnExit = false) {
         val userToken by LocalPropertiesStorage.getLive(USER_TOKEN).collectAsState(null)
