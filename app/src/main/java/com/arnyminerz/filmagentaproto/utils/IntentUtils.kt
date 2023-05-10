cc1596d600b0a6b5ac2e67887563d7cc0f309f86
Arnau Mora <arnyminerz@proton.me>
2023-05-10 18:55:58 +0200

    Migrated `launchCalendarInsert` to `LocalDateTime`
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- app/src/main/java/com/arnyminerz/filmagentaproto/utils/IntentUtils.kt
+++ app/src/main/java/com/arnyminerz/filmagentaproto/utils/IntentUtils.kt
@@ -7,28 +7,35 @@ import android.provider.CalendarContract
 import android.provider.CalendarContract.Events
 import androidx.activity.result.ActivityResultLauncher
 import com.arnyminerz.filmagentaproto.R
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZoneOffset
 import java.util.Date
 import kotlin.reflect.KClass
 
-fun Context.launchCalendarInsert(begin: Date, end: Date? = null) {
+fun Context.launchCalendarInsert(begin: LocalDateTime, end: LocalDateTime? = null) {
     val intent = Intent(Intent.ACTION_INSERT).apply {
         setDataAndType(Events.CONTENT_URI, "vnd.android.cursor.item/event")
         putExtra(Events.TITLE, getString(R.string.calendar_trebuchet_title))
         putExtra(Events.DESCRIPTION, getString(R.string.calendar_trebuchet_description))
-        putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, begin.time)
-        putExtra(CalendarContract.EXTRA_EVENT_END_TIME, end?.time ?: begin.time)
+        val beginEpoch = begin.atZone(ZoneId.systemDefault()).toEpochSecond()
+        val endEpoch = end?.atZone(ZoneId.systemDefault())?.toEpochSecond()
+        putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginEpoch)
+        putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endEpoch ?: beginEpoch)
         putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, false)
     }
     startActivity(intent)
 }
 
-fun Context.launchCalendarInsert(begin: Date, end: Date? = null, title: String, description: String?) {
+fun Context.launchCalendarInsert(begin: LocalDateTime, end: LocalDateTime? = null, title: String, description: String?) {
     val intent = Intent(Intent.ACTION_INSERT).apply {
         setDataAndType(Events.CONTENT_URI, "vnd.android.cursor.item/event")
         putExtra(Events.TITLE, title)
         putExtra(Events.DESCRIPTION, description)
-        putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, begin.time)
-        putExtra(CalendarContract.EXTRA_EVENT_END_TIME, end?.time ?: begin.time)
+        val beginEpoch = begin.atZone(ZoneId.systemDefault()).toEpochSecond()
+        val endEpoch = end?.atZone(ZoneId.systemDefault())?.toEpochSecond()
+        putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginEpoch)
+        putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endEpoch ?: beginEpoch)
         putExtra(CalendarContract.EXTRA_EVENT_ALL_DAY, false)
     }
     startActivity(intent)
