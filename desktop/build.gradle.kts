ebb825e5ae14b604e46b139bb79235f5f1677d38
Arnau Mora <arnyminerz@proton.me>
2023-10-02 08:17:28 +0200

    Upgraded versions
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 
 plugins {
-    kotlin("jvm") version "1.8.20"
-    id("org.jetbrains.compose") version "1.4.0"
+    kotlin("jvm") version "1.9.10"
+    id("org.jetbrains.compose") version "1.5.2"
     id("com.google.devtools.ksp")
 }
 
